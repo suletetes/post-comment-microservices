9b7bd2eea21ff7ed65117fb0813701a974b5f046
suleabdul <sulea841@gmail.com>
2024-06-28 17:11:46 +0100

    second commit

--- client/PostList.js
+++ client/PostList.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import axios from "axios";
+import axios, {post} from "axios";
 
 export default () => {
     const [posts, setPosts] = useState({})
@@ -13,7 +13,19 @@ export default () => {
         fetchPosts();
     }, []);
 
-    console.log()
+    console.log(posts)
+    const renderedPosts = Object.values(posts).map(posts => {
+        return (
+            <div className="card" style={{width: '30%', marginBottom: '20px'}} key={post.id}>
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                </div>
+            </div>
+        )
+    })
 
-    return </div>;
+    return (
+        <div className="d-flex flex-row flex-wrap justify-content-between">
+            {renderedPosts}
+        </div>)
 }
