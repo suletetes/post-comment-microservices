fc6871fef5f032a960588a621d93c701eb765996
suleabdul <sulea841@gmail.com>
2024-06-28 17:20:24 +0100

    second commit

--- client/PostList.js
+++ client/PostList.js
@@ -1,24 +1,28 @@
 import React, {useState, useEffect} from "react";
 import axios, {post} from "axios";
+import CommentCreate from "./src/CommentCreate";
+import CommentList from "./src/CommentList";
 
 export default () => {
-    const [posts, setPosts] = useState({})
+    const [post, setPost] = useState({})
 
     const fetchPosts = async () => {
         const res = await axios.get('http://localhost:4000/posts')
-        setPosts(res.data)
+        setPost(res.data)
     }
 
     useEffect(() => {
         fetchPosts();
     }, []);
 
-    console.log(posts)
-    const renderedPosts = Object.values(posts).map(posts => {
+    console.log(post)
+    const renderedPosts = Object.values(post).map(posts => {
         return (
             <div className="card" style={{width: '30%', marginBottom: '20px'}} key={post.id}>
                 <div className="card-body">
                     <h3>{post.title}</h3>
+                    <CommentList/>
+                    <CommentCreate postId={post.id}/>
                 </div>
             </div>
         )
