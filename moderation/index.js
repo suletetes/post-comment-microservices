addac980a5934d67fb7047829b851bbaa03d7e4f
suleabdul <sulea841@gmail.com>
2024-06-29 17:36:08 +0100

    updated

--- moderation/index.js
+++ moderation/index.js
@@ -9,7 +9,20 @@ app.use(bodyParser.json())
 
 
 app.post('/events', (req, res) => {
+    const {type, data} = req.body;
 
+    if (type === 'commentCreate') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved';
+        await axios.post('http://localhost:4005/events', {
+            type: 'commentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        })
+    }
 })
 
 
