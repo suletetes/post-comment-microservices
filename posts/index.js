4e35aba5bb26b8778d9405af7606c53bba129119
suleabdul <sulea841@gmail.com>
2024-06-29 14:46:47 +0100

    updating post

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {randomBytes} = require('crypto')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express();
 app.use(bodyParser.json())
@@ -15,13 +16,20 @@ app.get('/post', (req, res) => {
 
 });
 
-app.post('/post', (req, res) => {
+app.post('/post', async (req, res) => {
     const id = randomBytes(4).toString('hex')
     const {title} = req.body
 
     posts[id] = {
         id, title
     };
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'postCreated',
+        data: {
+            id, title
+        }
+    })
 
     res.send(201).send(posts[id]);
 
