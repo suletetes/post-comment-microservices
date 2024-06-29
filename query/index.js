0887e5c1b2247b0017762cd9194a1ff5581a4cf8
suleabdul <sulea841@gmail.com>
2024-06-29 21:17:08 +0100

    updated

--- query/index.js
+++ query/index.js
@@ -24,7 +24,7 @@ const handleEvent = (type, data) => {
         const {id, content, postId, status} = data
         const post = posts[postId]
         const comment = post.comments.find(comment => {
-            return comment.id = id
+            return comment.id === id
         })
         comment.status = status
         comment.content = content
@@ -36,6 +36,7 @@ app.get('/events', (req, res) => {
 })
 
 app.post('/posts', (req, res) => {
+    const {type, data} = req.body
     handleEvent(type, data)
     res.send({})
 })
@@ -43,10 +44,15 @@ app.post('/posts', (req, res) => {
 
 app.listen(4002, async () => {
     console.log('listening on port 4002')
-    await axios.get('https://localhost:4005/events')
-    for (let event of res.data) {
-        console.log('processing event:', event.type)
-        handleEvent(event.type, event.data)
+    try {
+        const res = await axios.get("http://localhost:4005/events");
 
+        for (let event of res.data) {
+            console.log("Processing event:", event.type);
+
+            handleEvent(event.type, event.data);
+        }
+    } catch (error) {
+        console.log(error.message);
     }
 })
